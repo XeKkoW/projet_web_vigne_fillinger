f534a139b713ba985dc56e4615618c742ab0df09
alex147569 <61408302+alex147569@users.noreply.github.com>
2021-11-27 16:35:22 +0100

    Début Nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import Home from './pages/Homes';
 import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Magueule</h1>
-      <Home/>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component = {Home} />
+      </Switch>
+    </BrowserRouter>
+      /*<div className="App">
+        <h1>Hello Magueule</h1>
+        <Home/>
+      </div>*/
   );
 }
 
