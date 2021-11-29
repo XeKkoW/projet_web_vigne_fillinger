1a01663f736e2a8669a7992b5e3cd4b58b4df064
XeKkoW <gabriel.vigne@edu.ece.fr>
2021-11-29 13:53:08 +0100

    comit API 2

--- frontend/src/components/Summoner.js
+++ frontend/src/components/Summoner.js
@@ -4,10 +4,10 @@ import { useState } from 'react/cjs/react.development';
 
 const Summoner = () => {
 	const [data, setData] = useState([])
-	var name = ""
+	var name = "thedarkgamer321"
 
 	const getSummoner = () => {
-		axios.get("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + { name } + "?api_key=RGAPI-9549364e-7295-42cd-892f-e71e28146f96")
+		axios.get("https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + {} + "?api_key=RGAPI-9549364e-7295-42cd-892f-e71e28146f96")
 			.then((res) => {
 				setData(res.data)
 
@@ -19,14 +19,6 @@ const Summoner = () => {
 
 	return (
 		<div>
-			<form action="">
-				<label for="Summoner name"> Entrer votre Summoner name</label>
-				<input
-					type="text"
-					value={name}
-					onChange={name.handleChange}
-				/>
-			</form>
 			{getSummoner}
 			{console.log(data)}
 		</div>
