85dc37a241fdceeff2c6f3fc022151c1285c2603
alex147569 <61408302+alex147569@users.noreply.github.com>
2021-11-29 16:12:10 +0100

    yavait ca aussi*

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,10 +1,9 @@
 import React from "react";
-import Navigation from "../Components/Navigation"
 
 const About = () => {
 return(
     <div className = "about">
-        <Navigation />
+        
         <h1>BlaBlaBla</h1>
     </div>
 )
