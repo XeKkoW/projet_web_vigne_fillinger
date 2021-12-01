8ff9bfb76f289fc8789271ba07668564258486ee
XeKkoW <gabriel.vigne@edu.ece.fr>
2021-12-01 23:42:52 +0100

    correction bug API 4

--- frontend/src/components/ShowRank.js
+++ frontend/src/components/ShowRank.js
@@ -3,13 +3,22 @@ import React from 'react';
 const ShowRank = (props) => {
 	const rank = props.rank
 	const tier = props.tier
-	console.log(tier);
-	console.log(rank);
-	return (
-		<div>
+	//console.log(tier);
+	//console.log(rank);
+	if (rank != "" && tier != "") {
+		return (
+			<div>
+				<h1>Rank : {tier} {rank} </h1>
+			</div>
+		);
+	}
+	else {
+		return (
+			<div>
 
-		</div>
-	);
+			</div>
+		);
+	}
 };
 
 export default ShowRank;
