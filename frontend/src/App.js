2164d1e83b9cb1ed4916110d9024651e22341083
XeKkoW <gabriel.vigne@edu.ece.fr>
2021-12-01 09:30:24 +0100

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
           <Route path="/page2" exact component={Page2} />
 
           <Route component={NotFound} />
-          
+
         </Switch>
       </BrowserRouter>
     </div>
